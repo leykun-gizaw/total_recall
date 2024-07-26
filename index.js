f46bf27af320c4793ec73a09369e42a6e0978f78
Leykun Gizaw <leykungizaw@gmail.com>
2024-07-26 12:35:13 +0300

    feat(index.js): Use express and add a route to list all database pages

--- index.js
+++ index.js
@@ -3,11 +3,36 @@
  * to.
  */
 
-require("dotenv").config();
+const dotenv = require("dotenv");
 const { Client, isFullPage } = require("@notionhq/client");
+const express = require("express");
 
+// Load environment variables from file
+dotenv.config();
 // Initializing a client
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
+// Create an express app
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, World!</h1>");
+});
+
+app.get("/db_pages", async (req, res) => {
+  const allDatabases = await getAllDatabases();
+  const allPages = [];
+
+  for (const db of allDatabases) {
+    const dbPages = await notion.databases.query({
+      database_id: db.id,
+    });
+    for (const page of dbPages.results) {
+      allPages.push(page);
+    }
+  }
+
+  res.json(allPages);
+});
 
 async function getAllDatabases() {
   const searchResult = await notion.search({
@@ -18,18 +43,6 @@ async function getAllDatabases() {
   return searchResult.results;
 }
 
-const database = getAllDatabases().then((databases) => {
-  for (const db of databases) {
-    const pages = notion.databases.query({
-      database_id: db.id,
-    });
-    pages.then((result) => {
-      for (const page of result.results) {
-        console.log(
-          page.properties["Question Titles"].title[0]["plain_text"],
-          page.properties["Revision Date"].date.start.split("T")[0]
-        );
-      }
-    });
-  }
+app.listen(8080, () => {
+  console.log("App listening on port 8080");
 });
